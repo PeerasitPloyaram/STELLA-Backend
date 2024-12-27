ac061166c957ae3a0d5550ae302ae61f0aacca87
Peerasit <peerasit.p@facgure.com>
2024-12-27 16:32:44 +0700

    [FIX] text_field Schema, Add Core Function to Create Database Name

--- milvus/schema.py
+++ milvus/schema.py
@@ -2,7 +2,7 @@ from pymilvus import CollectionSchema, FieldSchema, DataType
 
 id_field = FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True)  # Auto ID
 vector_field = FieldSchema(name="dense_vector", dtype=DataType.FLOAT_VECTOR, dim=1024)  # Vector Embedding dimension
-text_field = FieldSchema(name="text", dtype=DataType.VARCHAR, max_length=65535)  # Page Content
+text_field = FieldSchema(name="text", dtype=DataType.VARCHAR, max_length=8192)  # Page Content
 metadata_field = FieldSchema(name="metadata", dtype=DataType.JSON)  # Additional metadata
 
 schema_fields = [id_field, vector_field, text_field, metadata_field]
