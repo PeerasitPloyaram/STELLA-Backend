0ef0697abb37a8ac9c522fd3354c11a2dd4b323e
Peerasit <peerasit.p@facgure.com>
2025-02-12 20:05:16 +0700

    Test Hybrid Search and Remove Log

--- milvus/core.py
+++ milvus/core.py
@@ -1,11 +1,14 @@
 from pymilvus import connections, Collection, utility, db
 from langchain_core.documents import Document
 from langchain_milvus import Milvus
+
 from schema import DATA_SOURCE_SCHEMA, INDEX_PARAMS
 
-# import logging
-# logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s]  %(message)s")
-# logger = logging.getLogger("milvus_logger")
+# from pymilvus import WeightedRanker
+# from langchain_milvus.utils.sparse import BM25SparseEmbedding
+# from langchain_milvus.retrievers import MilvusCollectionHybridSearchRetriever
+# from schema import DATA_SOURCE_SCHEMA, DENSE_INDEX_PARAMS, SPARSE_INDEX_PARAMS
+
 
 class Core:
     def __init__(self,
@@ -27,19 +30,16 @@ class Core:
         system_prune: bool = False -> Drop the collection if it exists.
         verbose: bool = False -> Print the status of the database.
         """
-        # logger.info("[CORE] Initializing Milvus Database Core...")
+
         print("[CORE] Initializing Milvus Database Core...")
         self.database_name = database_name
         self.data_source_collection = collection_name
         self.data_source_schema = schema
 
         # Embedding Model
-        # logger.info("[DB] init Embedding Model...")
         print("[DB] init Embedding Model...")
         self.dense_embedding_model = dense_embedding_model
-        # logger.info("[DB] init Embedding Model Successfully.")
         print("[DB] init Embedding Model Successfully.")
-        self.sparse_embedding_model = ""
 
         self.initDataBase(system_prune=system_prune)
         
@@ -56,53 +56,50 @@ class Core:
         try:
             connections.connect(host="localhost", port="19530")
         except Exception as e:
-            # logger.error(f"[DB] Connection Error: {e}")
             print(f"[DB] Connection Error: {e}")
             return
         
         # list_db = db.list_database():
         if self.database_name in db.list_database():
-            # logger.info(f'[DB] Found Database: {self.database_name}')
-            print("f'[DB] Found Database: {self.database_name}'")
+            print(f'[DB] Found Database: {self.database_name}')
             connections.connect(host="localhost", port="19530", db_name=self.database_name)
         else:
-            # logger.info(f'[DB] Create Database: {self.database_name}')
             print(f'[DB] Create Database: {self.database_name}')
             db.create_database(self.database_name)
             connections.connect(host="localhost", port="19530", db_name=self.database_name)
         
         if utility.has_collection(self.data_source_collection):
-            # logger.info(f'[DB] Found Collection "{self.data_source_collection}".')
             print(f'[DB] Found Collection "{self.data_source_collection}".')
             
             if system_prune:
-                # logger.info(f'[DB] Drop Collection "{self.data_source_collection}"...')
                 print(f'[DB] Drop Collection "{self.data_source_collection}"...')
                 # utility.drop_collection(self.data_source_collection)
                 cl = Collection(name=self.data_source_collection)
                 print(f"{self.data_source_collection} has: {cl.num_entities} entities")
                 cl.drop()
-                # logger.info(f'[DB] Drop Collection "{self.data_source_collection}" Successfully.')
                 print(f'[DB] Drop Collection "{self.data_source_collection}" Successfully.')
             else:
                 self.collection = Collection(name=self.data_source_collection)
                 return
         
-        # logger.info(f'[DB] Create Collection "{self.data_source_collection}"')
         print(f'[DB] Create Collection "{self.data_source_collection}"')
         self.collection = Collection(
                                 name=self.data_source_collection,
                                 schema=self.data_source_schema,
-                                consistency_level="Strong"
+                                consistency_level="Strong",
+                                num_shards=4
                                 )
         self.collection.create_index("dense_vector", INDEX_PARAMS)
+        # self.collection.create_index("sparse_vector", SPARSE_INDEX_PARAMS)
         self.collection.flush()
 
-        # logger.info(f'[DB] Collection "{self.data_source_collection}" Is Ready.\n==========================================================================')
-        print(f'[DB] Collection "{self.data_source_collection}" Is Ready.\n==========================================================================')
+        print(f'[DB] Collection "{self.data_source_collection}" Is Ready.\n======================================================================')
 
     def getNumEntities(self):
         return self.collection.num_entities
+    
+    def describe(self):
+        return self.collection.describe()
 
     def initVectorStore(self, host:str="localhost", port:str="19530"):
         self.vector_store = Milvus(
@@ -123,11 +120,24 @@ class Core:
         )
         return self.vector_store
     
+    # def hybridRetreiver(self):
+    #     re = MilvusCollectionHybridSearchRetriever(
+    #         collection=self.collection,
+    #         rerank=WeightedRanker(0.3, 0.7),
+    #         anns_fields=["dense_vector", "sparse_vector"],
+    #         field_embeddings=[self.dense_embedding_model, BM25SparseEmbedding(corpus=[""])],
+    #         field_search_params=[DENSE_INDEX_PARAMS, SPARSE_INDEX_PARAMS],
+    #         top_k=4,
+    #         text_field="text",
+    #     )
+    #     return re
+    
     def add_document(self, documents: list[Document]):
         chunks = []
         for doc in documents:
             buffer = {
                 "dense_vector": self.dense_embedding_model.embed_query(doc.page_content),
+                # "sparse_vector": BM25SparseEmbedding(corpus=[""]).embed_query(doc.page_content),
                 "text": doc.page_content,
                 "metadata": doc.metadata
             }
--- milvus/schema.py
+++ milvus/schema.py
@@ -2,6 +2,7 @@ from pymilvus import CollectionSchema, FieldSchema, DataType
 
 id_field = FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True)  # Auto ID
 vector_field = FieldSchema(name="dense_vector", dtype=DataType.FLOAT_VECTOR, dim=1024)  # Vector Embedding dimension
+# sparse_field = FieldSchema(name="sparse_vector", dtype=DataType.SPARSE_FLOAT_VECTOR)
 text_field = FieldSchema(name="text", dtype=DataType.VARCHAR, max_length=8192)  # Page Content
 metadata_field = FieldSchema(name="metadata", dtype=DataType.JSON)  # Additional metadata
 
@@ -28,3 +29,20 @@ INDEX_PARAMS = {
     },
     "metric_type": "L2"
 }
+
+# DENSE_INDEX_PARAMS = {
+#     "index_type": "IVF_PQ",
+#     "params": {
+#         "nlist": 128,
+#         "m": 4
+#     },
+#     "metric_type": "IP"
+# }
+
+# SPARSE_INDEX_PARAMS = {
+#     "index_type": "SPARSE_INVERTED_INDEX",
+#     "params": {
+#         "inverted_index_algo": "DAAT_MAXSCORE"
+#     },
+#     "metric_type": "IP"
+# }
