1743925ea5a79b196b718d1cb906ebf2c859e3de
Peerasit <peerasit.p@facgure.com>
2025-02-19 20:56:03 +0700

    Update SQL Function services

--- db/service.py
+++ db/service.py
@@ -14,42 +14,73 @@ connection = mariadb.connect(
 )
 
 
-
-def createNewStorage(collection_name, partition_name):
-    sql = "INSERT INTO collection_storage(collection_name, partition_name) VALUES (%s, %s);"
-
-    try:
-        cursor = connection.cursor()
-        values = (collection_name, partition_name)
-
-        cursor.execute(sql, values)
-        id = cursor.lastrowid
-        connection.commit()
-
-        return id
-
-    except mariadb.IntegrityError as e:
-        print("[DB][ERROR]", e)
-        connection.rollback()
-
-    finally:
-        # connection.close()
-        pass
-
-
-
-def insertData(name, type_id, collection_name, partitiion_name):
-    storage_id = createNewStorage(collection_name, partitiion_name)
-
-    sql = "INSERT INTO documents (name, is_active, storage_id, type_id) VALUES (%s, %d, %d, %d)";
-    values = (name, True, storage_id, type_id)
+# Create New Location Storage of Collection name and Partition name
+def createNewLocStorage(collection_name:str, partition_name:str):
+    sql = "INSERT INTO location_storages (collection_name, partition_name) VALUES (%s, %s)"
+    values = (collection_name, partition_name)
 
     cursor = connection.cursor()
     cursor.execute(statement=sql, data=values)
     connection.commit()
+    return cursor.lastrowid
 
-# Find Documents Collection and Partition in Vector DB
-def findDocumentLoc(names: list)-> dict:
+#  Insert Data Related About company
+def insertCompanyData(sector_id:int, abbr:str, collection_name: str, partition_name: str, company_name_th=None, company_name_en=None)-> None:
+    sql = """INSERT INTO companies (abbr, is_active, sector_id, location_storage_id, company_name_th, company_name_en)
+    VALUES (%s, %d, %d, %d, %s, %s)
+    """
+
+    name_th_insert = None
+    name_en_insert = None
+    if company_name_th != None:
+        name_th_insert = company_name_th
+    if company_name_en != None:
+        name_en_insert = company_name_en
+
+    # try:
+    loc_id = createNewLocStorage(collection_name=collection_name, partition_name=partition_name)
+    values = (abbr, 1, sector_id, loc_id, name_th_insert, name_en_insert)
+    cursor = connection.cursor()
+    cursor.execute(statement=sql, data=values)
+    connection.commit()
+    print("[DB] Commit new Company")
+    # except:
+        #     print("error")
+
+
+def insertGeneralData(document_name:str, collection_name: str, partition_name: str)-> None:
+    sql = """INSERT INTO documents (document_name, is_active, location_storage_id)
+    VALUES (%s, %d, %d)
+    """
+    # try:
+    loc_id = createNewLocStorage(collection_name=collection_name, partition_name=partition_name)
+    values = (document_name, 1, loc_id)
+    cursor = connection.cursor()
+    cursor.execute(statement=sql, data=values)
+    connection.commit()
+    # except:
+        #     print("error")
+
+
+def getALLAbbrCompany()-> list:
+    sql = f"SELECT companies.abbr FROM companies WHERE is_active = 1"
+
+    cursor = connection.cursor()
+    cursor.execute(statement=sql)
+    buffer = cursor.fetchall()
+    return [c[0] for c in buffer]
+
+
+def getALLDocumentName()-> list:
+    sql = f"SELECT documents.document_name FROM documents WHERE is_active = 1"
+
+    cursor = connection.cursor()
+    cursor.execute(statement=sql)
+    buffer = cursor.fetchall()
+    return [c[0] for c in buffer]
+
+
+def findDataLoc(names: list)-> dict:
     if not names:
         return
     
@@ -57,10 +88,10 @@ def findDocumentLoc(names: list)-> dict:
     name_placeholders = ', '.join(prepare)
 
     sql = f"""
-    SELECT d.name, d.type_id, c.collection_name, c.partition_name
-    FROM documents d
-    JOIN collection_storage c ON d.storage_id = c.storage_id
-    WHERE d.name IN ({name_placeholders}) AND d.is_active = 1;
+    SELECT c.abbr, lc.collection_name, lc.partition_name
+    FROM companies c
+    JOIN location_storages lc ON c.location_storage_id = lc.location_storage_id
+    WHERE c.abbr IN ({name_placeholders}) AND c.is_active = 1;
     """
 
     cursor = connection.cursor()
@@ -74,16 +105,38 @@ def findDocumentLoc(names: list)-> dict:
     return output
 
 
-def getALLCompany()-> list:
-    sql = f"SELECT documents.name FROM documents WHERE is_active = 1 AND type_id = 1"
+def findCompanies(names: list):
+    prepare = [f"'{name}'" for name in names]
+    name_placeholders = ', '.join(prepare)
+
+    sql = f"SELECT companies.abbr FROM companies WHERE is_active = 1 AND companies.abbr IN ({name_placeholders})"
 
     cursor = connection.cursor()
     cursor.execute(statement=sql)
     buffer = cursor.fetchall()
-    return [c[0] for c in buffer]
 
+    a = []
+    for i in buffer:
+        a.append(i[0])
+    return a
+
+
+# def loadGeneralFileToLlm():
+#     sql = "SELECT documents.document_name, documents.description FROM documents WHERE is_active = 1;"
+#     cursor = connection.cursor()
+#     cursor.execute(statement=sql)
+#     buffer = cursor.fetchall()
+    
+#     a = []
+#     for i in buffer:
+#         temp = f"- **{i[0]}**: {i[1]}"
+#         a.append(temp)
+#     return a
 
 if __name__ == "__main__":
-    # insertData("coso", 2, "cnode_2", "coso")
-    # print(findDocumentLoc(["aot"]))
-    print(getALLCompany())
+    print(getALLAbbrCompany())
+    # print(getALLDocumentName())
+
+    # print(findDataLoc(["scb", "ndc", 'eee']))
+    # findCompanies(["bts", "aot"])
+    # print(loadGeneralFileToLlm())
